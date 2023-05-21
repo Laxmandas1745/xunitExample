868889b77323a76cb1ceab7625cfb6fa6144ad44
Laxman das <53378192+Laxmandas1745@users.noreply.github.com>
2023-05-21 18:13:53 +0530

    First Commit

---
+++ xunitExample.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using xunitExample.Controllers;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace xunitExample.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void HomeController_Index_ValidResult()
+        {
+            HomeController controller = new HomeController();
+            string expectedResult1 = "I am learning DevOps";
+            string expectedResult2 = "DevOps is being learnt by me";
+            string result = controller.Index();
+            Assert.Equal(expectedResult1,result);
+            Assert.Equal(expectedResult2, result);
+        }
+    }
+}
---
+++ xunitExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+namespace xunitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        
+        public string Index()
+        {
+            return "I am learning DevOps";
+        }
+    }
+}
